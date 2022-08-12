e01215f5272d2985ba13e3b3f54aa7492ec699f6
pbesra <psbesra@gmail.com>
2022-08-12 20:58:41 +0530

    table added

--- site.js
+++ site.js
@@ -1,1 +1,18 @@
-console.log("prakash besra");
+var columns = ["id", "name", "last", "description"];
+var editableColumns = ["name", "description"];
+var tableId = "table-id";
+var tableRows = document.getElementById("table-id").querySelector('tbody').querySelectorAll('tr');
+var createOnClickHandler = function (id, event) {
+    console.log(id);
+    console.log(event);
+}
+for (var i = 0; i < tableRows.length; i++) {
+    var tds = tableRows[i].querySelectorAll('td');
+
+    const id = tableRows[i].id;
+    for (var j = 0; j < tds.length; j++) {
+        tds[j].ondblclick = (event) => {
+            createOnClickHandler(id, event);
+        };
+    }
+}
